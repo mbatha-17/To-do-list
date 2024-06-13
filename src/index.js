ef79f8e41819281604fde55a4b35fb3167c3eb4a
mbatha-17 <jullymbatha17@gmail.com>
2024-06-13 22:12:43 +0300

    Imported addtask and renderTasks from tasks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import './style.css';
+import { addTask, renderTasks } from './tasks'
 
 const tasks = [
   { description: 'clean house', completed: true, index: 0 },
   { description: 'wash dishes', completed: true, index: 1 },
   { description: 'watch movie', completed: false, index: 2 },
 ];
+document.getElementById('add-task-button').addEventListener('click', () => {
+  const taskInput = document.getElementById('new-task-input');
+  const taskDescription = taskInput.value.trim();
+  if (taskDescription) {
+    addTask(taskDescription);
+    taskInput.value = '';
+  }
+});
 
 const renderTasks = () => {
   const todoList = document.getElementById('todo-list');
